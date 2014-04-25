117e5b7b7b11b22bb8940a4a5b6f6c9fd45fa90b
dkeresteci <dkeresteci@gmail.com>
2014-04-25 16:20:18 -0400

    Version 1, pagination working

--- app/scripts/collections/items.js
+++ app/scripts/collections/items.js
@@ -5,9 +5,15 @@ Sites.Collections = Sites.Collections || {};
 (function () {
     'use strict';
 
-    Sites.Collections.Items = Backbone.Collection.extend({
+    Sites.Collections.Items = Backbone.PageableCollection.extend({
 
         model: Sites.Models.Item,
+
+        mode: "client",
+
+        state: {
+        	pageSize: 10
+        },
 
         comparator: function(item){
         	return new Date(item.get('Date')); 
--- app/scripts/views/itemslist.js
+++ app/scripts/views/itemslist.js
@@ -17,22 +17,28 @@ Sites.Views = Sites.Views || {};
 
         className: '',
 
-        events: {},
+        events: {
+        'click .newpage': 'getPage',
+
+        },
 
         initialize: function (data) {
-
+            var that = this;
             this.collection = new Sites.Collections.Items(data);
             console.log(this.collection);
+
             this.render();
         },
 
         render: function () {
             var self = this;
             $('#accordion').empty();
-            console.log(this.collection.models);
             _.each(this.collection.models, function(item){
                 self.renderItem(item);
             }, this);
+
+            this.setPagination(this.collection.state.currentPage);
+            this.setTotals(this.collection.state.currentPage);
 
         },
 
@@ -42,6 +48,52 @@ Sites.Views = Sites.Views || {};
             });
 
             $('#accordion').append(newitem.render().el);
+        },
+
+        nextPage: function(){
+            this.collection.getNextPage();
+            this.render();
+        },
+
+        prevPage: function(){
+            this.collection.getPreviousPage();
+            this.render();
+        },
+
+        getPage: function(e){
+            var n = $(e.target).data('page');
+
+            if(n == 'next'){ 
+                this.nextPage();
+            }else if (n == 'prev'){
+                this.prevPage();
+            }else{
+                this.collection.getPage(n);
+                this.render();
+            }
+
+            console.log("data of item: " + n)
+        },
+
+        setPagination: function(page){
+            $('#currentpage').html(page);
+            var pagemax = this.collection.state.totalPages;
+            $('.newpage').each( function(){
+               $(this).data('page') == page ? $(this).parent('li').addClass('active') : $(this).parent('li').removeClass('active'); 
+            });
+
+            page >= (pagemax -1) ? $( ".newpage" ).last().parent('li').addClass('disabled') : $( ".newpage" ).last().parent('li').removeClass('disabled');
+            page == 1 ? $( ".newpage" ).first().parent('li').addClass('disabled') : $( ".newpage" ).first().parent('li').removeClass('disabled');
+
+        },
+
+        setTotals: function(page){
+            var pagesize = this.collection.state.pageSize
+            var last = page*pagesize;
+            var first = last - pagesize + 1;
+            $('#totals').html(
+                first + ' to ' + last + ' of '  + this.collection.state.totalRecords
+            );
         }
 
     });
