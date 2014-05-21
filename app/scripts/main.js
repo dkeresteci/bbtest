fe4d2996a30f10d8361cfb2c23828d4e5b65c462
dkeresteci <dkeresteci@gmail.com>
2014-05-21 00:41:05 -0400

    Added Friimi Colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,15 +8,16 @@ window.Sites = {
     Routers: {},
     init: function () {
 
-      $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-         options.url = 'http://expresstestdk.herokuapp.com' + options.url;
-      });
+      // $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+      //    options.url = 'http://expresstestdk.herokuapp.com' + options.url;
+      // });
 
-        var jqxhr = $.getJSON( "/items", function(data) {
-                directory = new Sites.Views.Itemslist(data);
-              });
+      //   var jqxhr = $.getJSON( "/items", function(data) {
+      //           directory = new Sites.Views.Itemslist(data);
+      //         });
               
-        
+        directory = new Sites.Views.Itemslist(directoryData);
+
         'use strict';
         console.log('Hello from Backbone!');
     }
